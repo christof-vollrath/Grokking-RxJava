6e9fee65af77fd2a9310a96f44d7ce753a6db037
Christof Vollrath <christof@taobits.net>
2016-07-29 22:17:44 +0200

    Still not async

--- src/main/kotlin/Part3_Errors_and_Concurrency.kt
+++ src/main/kotlin/Part3_Errors_and_Concurrency.kt
@@ -9,7 +9,7 @@ import java.util.regex.Pattern
 
 // See see http://blog.danlew.net/2014/09/22/grokking-rxjava-part-2/
 
-fun getUrls(): Observable<List<String>> = Observable.just(listOf("http://www.heise.de", "http://www.golem.de", "http://www.arstechnica.com", "http://unkown_url", "http://wired.com"))
+fun getUrls(): List<String> = listOf("http://www.heise.de", "http://www.golem.de", "http://www.arstechnica.com", "http://unkown_url", "http://wired.com")
 
 
 fun readTitle(urlStr: String): String? = grepTitle(readUrl(urlStr)) + " " +Thread.currentThread().getName()
@@ -17,17 +17,23 @@ fun readTitle(urlStr: String): String? = grepTitle(readUrl(urlStr)) + " " +Threa
 internal fun grepTitle(urlText: String): String? =
     Regex("<title>(.*)</title>", RegexOption.DOT_MATCHES_ALL).find(urlText)?.groups?.get(1)?.value
 
-internal fun readUrl(urlStr: String): String =
-    URL(urlStr).readText() // Can throw errors for unknown urls
+internal fun readUrl(urlStr: String): String {
+    val startTime = System.currentTimeMillis()
+    println("Start reading $urlStr")
+    val text = URL(urlStr).readText() // Can throw errors for unknown urls
+    println("Reading $urlStr took ${System.currentTimeMillis()-startTime} ms")
+    return text
+}
 
 fun printUrls() {
-    val urlListObservable: Observable<List<String>> = getUrls() // Observable with a list of urls
+    val urlListObservable: Observable<List<String>> = Observable.just(getUrls()) // Observable with a list of urls
     val urlsObservable: Observable<String> = urlListObservable.flatMap { // Observable emitting individual urls
         Observable.from(it)
     }
-    urlsObservable.subscribe {
-        println("Url: $it")
-    }
+    //urlsObservable.subscribe {
+    //    println("Url: $it")
+    //}
+    urlsObservable.toBlocking().forEach { println("Url: $it") }
 }
 
 // Check http://tomstechnicalblog.blogspot.de/2015/11/rxjava-achieving-parallelization.html
@@ -35,33 +41,31 @@ fun printUrls() {
 fun <T> Observable<T>.debug(s: String): Observable<T> = this.doOnNext({println("onNext $s: ${it} - [${Thread.currentThread().getName()}]")})
 
 fun printTitles() {
-    val urlListObservable: Observable<List<String>> = getUrls() // Observable with a list of urls
-    urlListObservable.flatMap { Observable.from(it) }// Observable emitting individual urls
-            .debug("flatMap Url")
-        .flatMap { Observable.just(it) }
-            .debug("list")
-        .subscribeOn(Schedulers.io())
-            .debug("subscribeOn io")
-        .map { readTitle(it) }
-            .debug("map read title")
-            .onErrorReturn { null }
-            .debug("onError null")
-        .filter { it != null }
-            .debug("filter null")
-        .toBlocking().forEach { println("Url: $it") }
+    val startTime = System.currentTimeMillis()
+    val urlList: Observable<List<String>> = Observable.just(getUrls()) // Observable with a list of urls
+    val urls: Observable<String> = urlList.flatMap { Observable.from(it) }// Observable emitting individual urls
+                                    .debug("flatMap Url")
+    val titles: Observable<String?> = urls.concatMap {
+                    Observable.just(it)
+                    .observeOn(Schedulers.computation())
+                    .debug("observeOn computation")
+                    .map {readTitle(it)}
+                    .debug("map readTitle")
+            }
+            .debug("concatMap parallel")
+            .observeOn(Schedulers.io())
+            .debug("observeOn io")
+    val titlesWithoutErrors: Observable<String?> = titles.onErrorReturn { null }
+                                        .debug("onError null")
+                                        .filter { it != null }
+                                        .debug("filter null")
+    titlesWithoutErrors.toBlocking().forEach { println("Url: $it") }
+    println("Took ${System.currentTimeMillis()-startTime} ms")
+    //Thread.sleep(10000)
 }
 
-fun showThreads() {
-    Observable.just(1,2,3,4)
-    .map { "$it: Map on thread: ${Thread.currentThread().getName()}"}
-    .subscribeOn(Schedulers.computation())
-    .observeOn(Schedulers.io())
-    .subscribe() { println("$it subscribe on: ${Thread.currentThread().getName()}")}
-}
 
 fun main(args: Array<String>) {
     printUrls()
     printTitles()
-    showThreads()
-    Thread.sleep(20000)
 }
